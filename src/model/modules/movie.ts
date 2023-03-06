850e863aacb3f20eeb55b0c2938e69f95fc5ffef
wanggang <wanggang@dyrs.com.cn>
2023-03-06 13:49:11 +0800

    1

--- src/model/modules/movie.ts
+++ src/model/modules/movie.ts
@@ -1,7 +1,26 @@
 export default <AppItem[]>[
   {
-    homepage: 'https://pan666.cn/',
-    icon: 'https://pan666.cn/favicon.ico',
+    homepage: [
+      { href: 'https://newxiaozhan.com/', title: '小站' },
+      { href: 'https://yunpan1.24kdh.cn/', title: '云盘资源分享社区' },
+      {
+        href: 'http://xdys88.com.cn/#/',
+        title: '羊驼小站',
+      },
+      {
+        href: 'https://www.yunpanziyuan.com',
+        title: '羊驼小站',
+      },
+      {
+        href: 'https://www.google.com/search?q=%E9%98%BF%E9%87%8C%E4%BA%91%E7%9B%98%E8%B5%84%E6%BA%90&ei=bx4AZL-gHtaghwPEqbugAw&ved=0ahUKEwj_k6ODrbz9AhVW0GEKHcTUDjQQ4dUDCA8&uact=5&oq=%E9%98%BF%E9%87%8C%E4%BA%91%E7%9B%98%E8%B5%84%E6%BA%90&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgYIABAHEB4yBQgAEIAEOgoIABBHENYEELADSgQIQRgAUGtYa2D3BGgBcAF4AIABgwGIAYMBkgEDMC4xmAEAoAEByAEKwAEB&sclient=gws-wiz-serp',
+        title: '谷歌搜索',
+      },
+      {
+        href: 'https://www.google.com/search?q=%E9%98%BF%E9%87%8C%E5%B0%8F%E7%AB%99&ei=HVIFZKmMHZiDoASy2Z2QAw&ved=0ahUKEwjpjumBo8b9AhWYAYgKHbJsBzIQ4dUDCA8&uact=5&oq=%E9%98%BF%E9%87%8C%E5%B0%8F%E7%AB%99&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzIFCAAQgAQyBQgAEIAEMgUIABCABDoKCAAQRxDWBBCwA0oECEEYAFA-WMIBYIMDaAFwAXgAgAGdAYgBtgKSAQMwLjKYAQCgAQHIAQrAAQE&sclient=gws-wiz-serp',
+        title: '谷歌搜索',
+      },
+    ],
+    icon: 'https://newxiaozhan.com/favicon.ico',
     keywords: [],
     name: '阿里云盘资源共享站',
   },
