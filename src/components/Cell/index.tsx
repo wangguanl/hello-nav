db258f9794fa65d31f58d06c40b95e112555d964
wanggang <wanggang@dyrs.com.cn>
2023-11-03 11:11:15 +0800

    1

--- src/components/Cell/index.tsx
+++ src/components/Cell/index.tsx
@@ -45,52 +45,29 @@ const single = ({ homepage, icon, repository, name, darkInvert }: AppItem) => (
 
 const Cell = ({ homepage, icon, repository, name, darkInvert }: AppItem) => (
   <li className="cell">
-    {Array.isArray(homepage) ? (
-      homepage.length > 1 ? (
-        <Dropdown
-          menu={{
-            items: homepage.map(({ href, title }) => ({
-              key: href,
-              label: (
-                <a className="app" href={href} target="_blank">
-                  {title}
-                </a>
-              ),
-            })),
-          }}
-          placement="bottomLeft"
-          arrow
-        >
-          <a className="app" href="#!" title={name}>
-            <div className="img-box">
-              <img
-                src={getImgSrc(icon)}
-                className={darkInvert ? 'dark-invert' : ''}
-                alt={name}
-              />
-            </div>
-            <p className="title">{name}</p>
-            {repository && (
-              <div
-                onKeyDown={() => {}}
-                onClick={e => onCornerClick(e, repository)}
-                className="corner"
-              >
-                <div className="corner-icon-wrap">
-                  <img
-                    className="corner-icon"
-                    draggable={false}
-                    src={gitHubIcon}
-                    alt=""
-                  />
-                </div>
-              </div>
-            )}
-          </a>
-        </Dropdown>
-      ) : (
-        single({ homepage: homepage[0].href, icon, repository, name, darkInvert })
-      )
+    {Array.isArray(homepage) && homepage.length > 1 ? (
+      <Dropdown
+        menu={{
+          items: homepage.map(({ href, title }) => ({
+            key: href,
+            label: (
+              <a className="app" href={href} target="_blank">
+                {title}
+              </a>
+            ),
+          })),
+        }}
+        placement="bottomLeft"
+        arrow
+      >
+        {single({
+          homepage: homepage[0].href,
+          icon,
+          repository,
+          name,
+          darkInvert,
+        })}
+      </Dropdown>
     ) : (
       single({ homepage, icon, repository, name, darkInvert })
     )}
